12bbea813591fd23f27398ef10e7af0a8f4886c4
Enrique Chimal Juarez <110999034+Echimalj@users.noreply.github.com>
2026-04-24 15:25:26 -0500

    Create advanced_analysis_examples.R

---
+++ scripts/advanced_analysis_examples.R
@@ -0,0 +1,13 @@
+source("R/check_dependencies.R")
+source("R/module_score_utils.R")
+source("R/differential_expression_utils.R")
+source("R/speckle_utils.R")
+
+check_required_packages(c(
+  "Seurat",
+  "dplyr",
+  "ggplot2",
+  "ggpubr"
+))
+
+AD_CAA <- readRDS("checkpoints/AD_CAA_final_processed.rds")
