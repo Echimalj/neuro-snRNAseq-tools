febde9d8132691add240aab33ab606a0edaf1ffd
Enrique Chimal Juarez <110999034+Echimalj@users.noreply.github.com>
2026-04-24 15:41:42 -0500

    Update advanced_analysis_examples.R

--- scripts/advanced_analysis_examples.R
+++ scripts/advanced_analysis_examples.R
@@ -13,4 +13,31 @@ check_required_packages(c(
 AD_CAA <- readRDS("checkpoints/AD_CAA_final_processed.rds")
 
 #Module Scores Utilities (DAA, DAM, DAO)
+# Disease-associated astrocyte module
+daa_genes <- c(
+  "AMIGO2", "FBLN5", "FKBP5", "GBP2", "PSMB8", "SERPING1",
+  "SRGN", "CD44", "HSPB1", "LTBP1", "TNC"
+)
+
+astro <- subset(
+  AD_CAA,
+  idents = c("Astrocytes1", "Astrocytes2", "Astrocytes3", "Astrocytes4", "Astrocytes5")
+)
+
+astro <- add_gene_module_score(
+  seu = astro,
+  gene_set = daa_genes,
+  module_name = "DAA_Score",
+  assay = "RNA"
+)
+
+daa_pct <- percent_cells_expressing_module(
+  seu = astro,
+  gene_set = daa_genes,
+  threshold = 5,
+  assay = "RNA",
+  celltype_col = "cellclass",
+  group_col = "FDX"
+)
+
 
